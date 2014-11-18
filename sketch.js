adb6db8b0d243f5cbcbc9073c0ec2a0ca416adc4
heesuchoi1 <heesu7563@gmail.com>
2014-11-18 21:16:21 +0900

    wetw

---
+++ sketch.js
@@ -0,0 +1,25 @@
+var count;
+
+
+function setup(){
+	creatCanvas(800,900);
+
+	count = 0;
+}
+
+function draw(){
+	background(0);
+
+
+	drawCircle(count,30);
+    count=count+1;
+    if(count>680){
+	count=0;
+	}
+}
+
+function drawCircle(x,y){
+	noStroke();
+	fill(255);
+	ellipse(50,50,50,50);
+}
